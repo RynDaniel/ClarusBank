244d455a89bb2378db9bfc8db5bb5615fee49a9c
DESKTOP-QCEF0R7\mryndnl <ikarusoftyazilim@gmail.com>
2021-07-29 22:55:53 +0300

    hocam bitir artık commit

---
+++ checkout.js
@@ -0,0 +1,32 @@
+// const queryString = window.location.search;
+// console.log(queryString);
+// const urlParams = new URLSearchParams(queryString);
+// console.log(urlParams);
+// const firstname = urlParams.get("firstname");
+// const lastname = urlParams.get("lastname");
+// alert("welcome " + firstname + " " + lastname);
+
+const updateProductQuantity = (input) => {
+    alert(input.value);
+}
+
+document.querySelectorAll(".product-quantity input").forEach(function(input){
+    input.addEventListener("change", function(){
+        updateProductQuantity(input);
+    });
+});
+
+const removeProduct = (button) => {
+    let productRow = button.parentElement.parentElement;
+    productRow.remove();
+}
+
+document.querySelectorAll(".product-removal button").forEach(function(button){
+    button.addEventListener("click", function(){
+        removeProduct(button);
+    });
+});
+
+
+
+
