9f3f89ce5a9c5ede30109ecd572896229a209541
DESKTOP-QCEF0R7\mryndnl <ikarusoftyazilim@gmail.com>
2021-08-12 20:48:06 +0300

    first lesson commit

--- checkout.js
+++ checkout.js
@@ -1,3 +1,62 @@
+// Product Constructor
+
+function Product(name, price, quantity){
+    console.log("Product's this");
+    console.log(this);
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+}
+
+function Display(){
+    console.log("Display's this");
+    console.log(this);
+    this.name = document.getElementById("name");
+    this.price = document.getElementById("price");
+    this.quantity = document.getElementById("quantity");
+}
+
+document.getElementById("customer-form").addEventListener("submit", function(e){
+    console.log("...event...");
+    console.log(e);
+    console.log("...form's this...");
+    console.log(this);
+    e.preventDefault();
+    ...
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const queryString = window.location.search;
 // console.log(queryString);
 // const urlParams = new URLSearchParams(queryString);
